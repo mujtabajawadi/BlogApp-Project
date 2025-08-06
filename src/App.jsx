d2a3057d38b3450f5d0a6c9f657953672d19436e
Muhammad Mujtaba Jawadi <mujtabafalcon986@gmail.com>
2025-08-06 12:46:42 +0500

    Configured redux-toolkit store and authSlice in the project to track the user's Login state.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-
-import './App.css'
+import { useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import obj_AuthService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import "./App.css";
+import { Header, Footer } from "./components";
+import {Outlet} from 'react-router-dom'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  useEffect(() => {
+    obj_AuthService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
 
-  return (
-  <h1>Hello Blog App with Appwrite</h1>
-  )
+  return !loading ?
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-500">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div> : <div>Loading...</div>;
 }
 
-export default App
+export default App;
